749101eadc5f40b3aa452c914b2042ab42a3b855
Sungwoo97 <gwaja97@gmail.com>
2024-12-05 17:28:59 +0900

    워드프레스 연습 2일차

--- functions.php
+++ functions.php
@@ -19,3 +19,39 @@ if (! function_exists('minimal_script')) {
   }
   add_action('wp_enqueue_scripts', 'minimal_script');
 }
+
+function register_my_menu()
+{
+  register_nav_menus(
+    array(
+      'header-menu' => __('Header Menu')
+    )
+  );
+}
+add_action('init', 'register_my_menu');
+
+add_theme_support('post-thumbnails');
+
+if (!function_exists('minimal_pagination')) {
+  function minimal_pagination()
+  {
+    $args = array(
+      'prev_next' => false,
+      'type' => 'array',
+    );
+    $pagination = paginate_links($args);
+    // print_r($pagination);
+    if (is_array($pagination) && count($pagination) > 0) {
+      echo '<p class="pagenation shadow">';
+      foreach ($pagination as $page) {
+        // echo $page;
+        if (strpos($page, 'current')) {
+          echo '<span class="secondary-btn active">' . $page . '</span>';
+        } else {
+          echo '<span class="secondary-btn">' . $page . '</span>';
+        }
+      }
+      echo '</p>';
+    }
+  }
+}
